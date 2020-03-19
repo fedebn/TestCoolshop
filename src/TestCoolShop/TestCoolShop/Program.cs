279bf293a8425a8306ce58c1d5a1fdf2aee8b252
Federico Lammendola <federico.lammendola@inpeco.com>
2020-03-19 12:37:31 +0100

    added person parse

--- src/TestCoolShop/TestCoolShop/Program.cs
+++ src/TestCoolShop/TestCoolShop/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,51 @@ namespace TestCoolShop
     {
         static void Main(string[] args)
         {
+            try
+            {
+                if (args == null)
+                    throw new ArgumentException("Input not inserted");
 
+                if(args.Length != 3)
+                    throw new ArgumentException("Check input parameter");
+
+                string path = args[0];
+                int id;
+                if (!int.TryParse(args[1], out id))
+                    throw new ArgumentException("Can't find id");
+                string name = args[2];
+
+                File.ReadLines(path)
+                     .Select(ParsePersonFromLine)
+                     .Where(p => p.Name == "Peter")
+                     .FirstOrDefault();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+        }
+
+        private static Person ParsePersonFromLine(string line)
+        {
+            string[] parts = line.Split(',');
+            Person p = new Person();
+
+            p.Id = Int32.Parse(parts[0]);
+            p.Name = parts[1];
+            p.Surname = parts[2];
+            DateTime BirthDate;
+            if (DateTime.TryParse(parts[3], out BirthDate))
+                p.BirthDate = BirthDate;
+            else
+                throw new ArgumentException("Can't find birthdate");
+
+            return p;
         }
     }
 }
