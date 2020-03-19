50310ff72601560442a39aa13e704c3d1c633e11
Federico Lammendola <federico.lammendola@inpeco.com>
2020-03-19 12:36:58 +0100

    added person class

---
+++ src/TestCoolShop/TestCoolShop/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCoolShop
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Person() { }
+
+        public Person(int id, string name, string surname, DateTime birthdate)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            BirthDate = birthdate;
+        }
+    }
+}
