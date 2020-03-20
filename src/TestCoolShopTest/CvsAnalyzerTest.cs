20c5d7478b3b54da271e9302c53d25eb4b0574ab
Federico Lammendola <federico.lammendola@inpeco.com>
2020-03-20 09:20:07 +0100

    Added test project

---
+++ src/TestCoolShopTest/CvsAnalyzerTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestCoolShop;
+namespace TestCoolShopTest
+{
+    [TestClass]
+    public class CvsAnalyzerTest
+    {
+        [TestMethod]
+        public void Routine()
+        {
+            var analyzer = new CsvAnalyzer();
+            analyzer.Path = @"myfile.csv";
+            analyzer.Separator = ',';
+
+            var result = analyzer.SelectByColummData(2, "Alberto");
+            Assert.AreEqual("3,Verdi,Alberto,03/08/1987;", result);
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void FileNotPresent()
+        {
+            var analyzer = new CsvAnalyzer();
+            analyzer.Path = @"myfileXX.csv";
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void ColumnNotPresent()
+        {
+            var analyzer = new CsvAnalyzer();
+            analyzer.Path = @"myfile.csv";
+            analyzer.Separator = ',';
+
+            var result = analyzer.SelectByColummData(5, "Alberto");
+            Assert.AreEqual("3,Verdi,Alberto,03/08/1987;", result);
+        }
+
+        [TestMethod]
+        public void NoResult()
+        {
+            var analyzer = new CsvAnalyzer();
+            analyzer.Path = @"myfile.csv";
+            analyzer.Separator = ',';
+
+            var result = analyzer.SelectByColummData(2, "Enzo");
+            Assert.AreEqual(string.Empty, result);
+        }
+    }
+}
---
+++ src/TestCoolShopTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestCoolShopTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestCoolShopTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6c06cc5f-2649-42f4-94e2-e397da5dad70")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
